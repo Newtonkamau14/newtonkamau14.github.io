57ed125699e01803312fa6124d67cc5505134b2a
Newtonkamau14 <newtonkamau14@gmail.com>
2025-03-06 06:15:51 +0300

    update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-const nextConfig:NextConfig = {
-  reactStrictMode: true,
+const nextConfig: NextConfig = {
   images: {
-    unoptimized: true, 
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        pathname: "/**",
+      },
+    ],
   },
-  assetPrefix: isProd ? '/newtonkamau14.github.io/' : '',
-  basePath: isProd ? '/newtonkamau14.github.io' : '',
-  output: 'export'
 };
 
 export default nextConfig;
